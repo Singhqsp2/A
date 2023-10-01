23d48008158946529f5846da7b14468a05216608
Abhibtm <abhisinghbtm@gmail.com>
2023-10-01 14:16:33 +0530

    FC

---
+++ B/src/test/java/test/Tc3.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc3 {
+	
+	@Test
+	public void m3()
+	{
+		Reporter.log("Tc3 Running",true);
+	}
+
+}
